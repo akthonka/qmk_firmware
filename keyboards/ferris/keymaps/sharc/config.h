f7f378ae26b072e22c31ac3966f0a14c04e8433b
Daniil <45102902+akthonka@users.noreply.github.com>
2022-04-15 22:58:38 +0200

    changes

--- keyboards/ferris/keymaps/sharc/config.h
+++ keyboards/ferris/keymaps/sharc/config.h
@@ -45,3 +45,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
   #define RGBLIGHT_SAT_STEP 8
   #define RGBLIGHT_VAL_STEP 8
 #endif
+
+// Enable handedness
+#define EE_HANDS
